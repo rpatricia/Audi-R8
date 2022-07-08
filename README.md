84faa19a26caaba3a5072a58c25bec69df6f7f72
Renata Pausini <rpatricia@outlook.com>
2022-07-08 12:26:27 -0300

    Adicionado querySellectorAll :sparkles:

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Audi-R8
+# Audi-R8 :sparkles:
  
+ ## Web sit Responsivo
+
+ Site responsivo teconologias usadas:<BR>
+ :check: HTML <BR>
+ :check: CSS <BR>
+ :check: JavaScript
+
+ <p> Desafios do projeto foi posicionar os elementos, usando flex box e grid, excutar o contador através do scroll da página para fazer as animações</p>
+
+ ## Pré-visualização do projeto:
+ []()
+
+ <div align="lefet"><img  src="https://i.ytimg.com/vi/woS0ViaOAlA/hqdefault.jpg" width=100%></div>
+
--- javascript.js
+++ javascript.js
@@ -1,27 +1,51 @@
+//Navbar
 const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".menu-btn");
 
+//Add click event
+
 menuBtn.addEventListener("click", () => {
+  //Toggle menu open class
   menu.classList.toggle("menu-open");
 });
 
+//Stats Counter
 const stats = document.querySelector(".stats");
-const counters = document.querySelector(".counter");
+const counters = document.querySelectorAll(".counter");
 
 let bol = false;
 
+/* Obter "deslocamento" da seção de estatísticas
+do "topo da página" para "altura do elemento" de estatísticas
+
+*/
 const sectionOffSet = stats.offsetTop + stats.offsetHeight;
 
+/*
+Inicia o contador quando a página
+é rolado para a seção de estatísticas
+*/
 window.addEventListener("scroll", () => {
+  /*
+obter o "deslocamento da página do topo" + a "altura 
+da tela inteira"
+ */
   const pageOffset = window.innerHeight + scrollY;
-
+  /*
+Execute a função updateCount se a página estiver
+rolado além da seção de estatísticas e executado apenas uma vez
+*/
   if (pageOffset > sectionOffSet && bol === false) {
     counters.forEach((counter) => {
       function updateCount() {
+        /*
+    Defina a variável e sobrescreva-a a cada
+  tempo com "+" taquigrafia
+   */
         const target = +counter.getAttribute("data-target");
         const speed = +counter.getAttribute("data-speed");
         const count = +counter.innerText;
-
+        /*Verifique se o alvo foi atingido*/
         if (count < target) {
           counter.innerText = count + 1;
           setTimeout(updateCount, speed);
@@ -31,8 +55,16 @@ window.addEventListener("scroll", () => {
       }
 
       updateCount();
+      /*
+     Execute a função apenas quebrando
+     uma das condições na instrução if
+    */
 
       bol = true;
+      /*
+     Isso impede que a função updateCount seja executada toda vez
+     você passa por esta seção
+    */
     });
   }
 });
